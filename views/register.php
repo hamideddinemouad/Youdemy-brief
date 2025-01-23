d9ed0c629b79699c8cadeab9a5dd0ad723252327
Hamideddine Mouad <hamideddine.mouad@gmail.com>
2025-01-23 07:49:10 +0100

    Add a newline at the end of register.php for better file formatting

--- views/register.php
+++ views/register.php
@@ -110,4 +110,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
+
 <?php require 'includes/footer.php'; ?>
